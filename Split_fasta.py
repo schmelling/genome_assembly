17bb68dbbb46c473fa2d8c3bb7f603be1be42f83
schmelling <schmelli@msu.edu>
2014-02-13 11:56:12 -0500

    Create Split_fasta.py

---
+++ Split_fasta.py
@@ -0,0 +1,22 @@
+#############################################################################
+#
+# This script provides a way to split a fasta file into to subfiles
+#
+# Please change the filenames for your purpose and the startswith argument
+#
+# Author Nicolas Schmelling
+#############################################################################
+
+
+new = open('1.fasta','w')
+with open('test.fasta','r') as tf:
+    count = 0
+    for line in tf:
+        if line.startswith(">DEF"):
+            count += 1
+        if count == 1:
+            new.close()
+            count = 0
+            new = open('2.fasta', 'w')
+        new.write(line)
+new.close()
