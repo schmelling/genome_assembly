0678352a8f81e2a3b71cf10f691d8a3a6b45306b
schmelling <schmelli@msu.edu>
2014-02-28 17:57:50 -0500

    Parsing BLAST/BLAT files

--- parsing_blast_blat_output.py
+++ parsing_blast_blat_output.py
@@ -1,33 +1,33 @@
 #############################################################################################
 #
 #   This script provides a way to parse a BLAST/BLAT (if in BLAST format) output text files
-#
-#   Please change the filenames for your purpose 
+# 
 #
 #   Author: Nicolas Schmelling
 #
 #############################################################################################
 
+def blast_parse(file, e, output):
 
-result_handle = open("Trinity.blast.txt")
-
-from Bio.Blast import NCBIStandalone
-blast_parser = NCBIStandalone.BlastParser()
-blast_iterator = NCBIStandalone.Iterator(result_handle, blast_parser)
-blast_record = next(blast_iterator)
-
-output = open('RNA_BLAST_result.txt', 'w')
-output.write('query title , alignment title , length , e value' + '\n')
-for blast_record in blast_iterator:
-    E_VALUE_THRESH = 0.04
-    for alignment in blast_record.alignments:
-        for hsp in alignment.hsps:
-            if hsp.expect < E_VALUE_THRESH:
-                output.write(str(blast_record.query) + ',')
-                output.write(str(alignment.title) + ',')
-                output.write(str(alignment.length) + ',')
-                output.write(str(hsp.expect) + '')
-                output.write('\n')
-
-
-output.close()
+    result_handle = open(file)
+    
+    from Bio.Blast import NCBIStandalone
+    blast_parser = NCBIStandalone.BlastParser()
+    blast_iterator = NCBIStandalone.Iterator(result_handle, blast_parser)
+    blast_record = next(blast_iterator)
+    
+    output = open(output, 'w')
+    output.write('query title\talignment title\tlength\te value' + '\n')
+    for blast_record in blast_iterator:
+        E_VALUE_THRESH = e
+        for alignment in blast_record.alignments:
+            for hsp in alignment.hsps:
+                if hsp.expect < E_VALUE_THRESH:
+                    output.write(str(blast_record.query[:18]) + ' \t')
+                    output.write(str(alignment.title) + '\t')
+                    output.write(str(alignment.length) + '\t')
+                    output.write(str(hsp.expect) + '')
+                    output.write('\n')
+    
+    
+    output.close()
