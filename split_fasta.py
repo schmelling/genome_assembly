79a5aa704cdb2b7a6619323d9cc1ca97f6030b87
Nicolas Schmelling <schmelli@msu.edu>
2014-03-03 16:49:18 -0500

    Splits a fasta file into two subfiles
    ... given an argument, when to split

--- split_fasta.py
+++ split_fasta.py
@@ -1,23 +1,22 @@
 #############################################################################
 #
-#   This script provides a way to split a fasta file into to subfiles
+#   This script provides a way to split a fasta file into to two subfiles
 #
-#   Please change the filenames for your purpose and the startswith argument
 #
 #   Author: Nicolas Schmelling
 #
 #############################################################################
 
-
-new = open('1.fasta','w')
-with open('test.fasta','r') as tf:
-    count = 0
-    for line in tf:
-        if line.startswith(">DEF"):
-            count += 1
-        if count == 1:
-            new.close()
-            count = 0
-            new = open('2.fasta', 'w')
-        new.write(line)
-new.close()
+def split(in_file, split_argument):
+    new = open('1.fasta','w')
+    with open('in_file','r') as tf:
+        count = 0
+        for line in tf:
+            if line.startswith("split_argument"):
+                count += 1
+            if count == 1:
+                new.close()
+                count = 0
+                new = open('2.fasta', 'w')
+            new.write(line)
+    new.close()
