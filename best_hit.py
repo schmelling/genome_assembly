0c98e0ff05b170d1ea524b20f4c5136bff9652ce
Nicolas Schmelling <schmelli@msu.edu>
2014-03-05 13:50:22 -0500

    Gives best hit from parsed BLAST file

--- best_hit.py
+++ best_hit.py
@@ -1,6 +1,7 @@
 '''
   
-  This code take a parsed BLAST file and returns the best hit
+  This code take a parsed BLAST file and returns the best hit,
+  if the best hit is 'hypothetical' or 'predicted' the code also returns the best hit with a know function
   
   Usage: python best_hit.py <input_file> <output_file>
 
@@ -14,14 +15,25 @@ def best_hit(file, output):
     
     best = open(output, 'w')
     r = open(file, 'r')
-    count = []
+    list = []
+    list2 = []
+    count = 0
     
     for line in r:
-        if line.split('\t')[0] in count:
-            continue
+        if line.split('\t')[0] in list:
+            if 'hypothetical' in line.split('\t')[1]:
+                continue
+            elif 'predicted' in line.split('\t')[1]:
+                continue
+            elif line.split('\t')[0] in list2:
+                continue
+            else:
+                best.write(line)
+                list2.append(line.split('\t')[0])
         else:
             best.write(line)
-            count.append(line.split('\t')[0])
+            list.append(line.split('\t')[0])
+            
     best.close()
     
 if __name__ == "__main__":
