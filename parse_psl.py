e0bc0c104f45a1cf2f7deeb97ce37d13c0ed9de2
Nicolas Schmelling <schmelli@msu.edu>
2014-03-17 15:46:58 -0400

    Separates contains by strand speficity

---
+++ parse_psl.py
@@ -0,0 +1,34 @@
+'''
+
+  Separates the strand specific contigs from each other, so gimme can work with each file separately.
+  
+  https://github.com/ged-lab/gimme
+
+  Usage: python parse_psl.py <psl file>
+
+  Author: Nicolas Schmelling
+
+'''
+
+import sys
+
+def psl_parse(file):
+    minus = open('minus_strand_seq.psl', 'w')
+    plus = open('plus_strand_seq.psl', 'w')
+    with open(file, 'rb') as infile:
+        hits = infile.read().split('\n')
+        for line in hits:
+            if len(line) >= 19:
+                if '+' in line:
+                    plus.write(line.lower())
+                    plus.write('\n')
+                elif '-' in line:
+                    minus.write(line.lower())
+                    minus.write('\n')
+    minus.close()
+    plus.close()
+    
+if __name__ == "__main__":
+    file = sys.argv[1]
+    
+    psl_parse(file)
