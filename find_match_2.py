e497ab738e3f84d074fc2032c6dc784db8bcb2c8
Nicolas Schmelling <schmelli@msu.edu>
2014-04-02 12:05:15 -0400

    Finding matches comparing fasta and BLAST results

--- find_match_2.py
+++ find_match_2.py
@@ -1,8 +1,8 @@
 '''
     Finding matches and no matches between a fasta and a BLAST file
-
-    Usage: python find_match_2.py <fasta_file> <blast_file> <kind of sequence in blast rna/prot>
-
+    
+    Usage: python find_match.py <fasta_file> <blast_file> <kind of sequence in blast rna/prot>
+    
     Author: Nicolas Schmelling
 
 '''
@@ -13,29 +13,29 @@ def compare(fasta_file, blast_file, kind):
     fasta_hit = {}
     with open(fasta_file, 'r') as infile:
         blast = open(blast_file)
-
+        
         match = open('match_'+kind+'.txt','w')
         no_match = open('no_match_'+kind+'.txt','w')
-
+        
         for line in blast:
             RNA_hit.update({line.split()[0]:line.split()[1]})
-
+        
         for line in infile:
             if line.startswith('>') and len(line) > 100:
                 fasta_hit.update({line.split(' ',1)[0].replace('>',''):line.split(' ',1)[1].replace('\n','')})
-
+                
         for hit in fasta_hit:
             if hit in RNA_hit:
-                match.write(str(hit) + '\t')
-                match.write(str(fasta_hit.get(hit)) + '\t')
-                match.write(str(RNA_hit.get(hit)) + '\n')
+                match.write(str(hit) + '\t' + str(fasta_hit.get(hit)) + '\t' + str(RNA_hit.get(hit)) + '\n')
             else:
-                no_match.write(str(hit) + '\t')
-                no_match.write(str(fasta_hit.get(hit)) + '\n')
-
+                no_match.write(str(hit) + '\t' + str(fasta_hit.get(hit)) + '\n')
+                
+        match.close()
+        no_match.close()
+                    
 if __name__ == "__main__":
     fasta_file = sys.argv[1]
     blast_file = sys.argv[2]
     kind = sys.argv[3]
-
-    compare(fasta_file, blast_file, kind)
+    
+    compare(fasta_file, blast_file, kind) 
