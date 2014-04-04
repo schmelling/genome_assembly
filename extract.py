ecbdb64f51387388efbbcf32aa602a5595565e22
Nicolas Schmelling <schmelli@msu.edu>
2014-04-04 11:24:48 -0400

    Extracting sequences from fasta file

--- extract.py
+++ extract.py
@@ -1,8 +1,8 @@
 '''
   
-  Extract all orthologs from fasta file
+  Extract special sequences from fasta file. You can specify which sequences get extracted by using the a separator.
   
-  Usage: python extract <fasta_file> <output_file>
+  Usage: python extract <fasta_file> <output_file> <separator>
   
   Author: Nicolas Schmelling
 
@@ -10,15 +10,16 @@
 from Bio import SeqIO
 import sys
 
-def extract(fasta_file, output_file):
+def extract(fasta_file, output_file, separator):
     with open(output_file,"w") as f:
         for seq_record in SeqIO.parse(fasta_file, "fasta"):
-            if 'ortho' in seq_record.description:
+            if separator in seq_record.description:
                 f.write(str(seq_record.description) + '\n')
                 f.write(str(seq_record.seq) + '\n')
                 
 if __name__ == "__main__":
     fasta_file = sys.argv[1]
     output_file = sys.argv[2]
+    separator = sys.argv[3]
     
-    extract(fasta_file, output_file) 
+    extract(fasta_file, output_file, separator) 
